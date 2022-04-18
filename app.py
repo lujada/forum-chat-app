726cffeb74f0e5d60cd5c63cd269d234e5eb4f45
lujada <dariusz.luja@helsinki.fi>
2022-04-18 12:07:26 +0300

    Added login handling. Added admin status to users in database and a radio button for it when creating a new user.

--- app.py
+++ app.py
@@ -23,38 +23,49 @@ def index():
 
 @app.route("/login", methods=["POST"])
 def login():
+    session["login_message"] = " "
     username = request.form["username"]
     password = request.form["password"]
-    sql = "SELECT id, password FROM users WHERE username=:username"
+    sql = "SELECT id, password, admin FROM users WHERE username=:username"
     result = db.session.execute(sql, {"username":username})
     user = result.fetchone()
     if not user:
-        print("invalid username")
+        session["login_message"] = "Invalid username!"
+        return redirect("/")
     else:
         hash_value = user.password
-        if check_password_hash(hash_value, password):
-            print('correct username and password')
+        if not check_password_hash(hash_value, password):
+            session["login_message"] = "Invalid password!"
+            return redirect("/")
         else:
-            print('invalid password')
-    session["username"] = username
-    session["userid"] = user.id
-    return redirect("/")
+            session["username"] = username
+            session["userid"] = user.id
+            session["admin"] = user.admin
+            session["login_message"] = " "
+            return redirect("/")
 
 @app.route("/createuser", methods=["POST"])
 def create_user():
     username = request.form["username"]
     password = request.form["password"]
+    admin = request.form["admin"]
     hash_value = generate_password_hash(password)
-    print(hash_value)
-    sql = "INSERT INTO users (username, password) VALUES (:username, :password)"
-    db.session.execute(sql, {"username":username, "password":hash_value})
+    sql = "INSERT INTO users (username, password, admin) VALUES (:username, :password, :admin) RETURNING id"
+    result = db.session.execute(sql, {"username":username, "password":hash_value, "admin":admin})
+    user_id = result.fetchone()[0]
     db.session.commit()
+    session["login_message"] = " "
+    session["username"] = username
+    session["userid"] = user_id
+    session["admin"] = admin
     return redirect("/")
 
 @app.route("/logout")
 def logout():
     del session["username"]
     del session["userid"]
+    del session["admin"]
+    session["login_message"] = " "
     return redirect("/")
 
 @app.route("/newuser")
@@ -67,16 +78,17 @@ def areas(name:str):
     result = db.session.execute(sql, {"name":name})
     area_id = result.fetchone()[0]
 
-    sql = "SELECT id, title, created FROM posts WHERE area_id=:area_id ORDER BY id DESC"
+    sql = "SELECT posts.id, posts.title, posts.created, users.username FROM posts, users WHERE area_id=:area_id AND posts.user_id = users.id ORDER BY id DESC"
     result = db.session.execute(sql, {"area_id":area_id})
     posts = result.fetchall()
     return render_template("Areas.html", posts=posts, name=name)
 
 @app.route("/<string:name>/<int:id>")
 def topic(name:str, id):
-    sql = "SELECT content, created FROM messages WHERE post_id=:id ORDER BY id DESC"
+    sql = "SELECT messages.content, messages.created, users.username FROM messages, users WHERE post_id=:id AND messages.user_id = users.id ORDER BY messages.id DESC"
     result = db.session.execute(sql, {"id":id})
     messages = result.fetchall()
+    print(messages, 'msgs')
     
     sql = "SELECT title, id FROM posts WHERE id=:id"
     result = db.session.execute(sql, {"id":id})
@@ -96,15 +108,14 @@ def new_post():
     result = db.session.execute(sql, {"name":name})
     area_id = result.fetchone()[0]
     user_id = session["userid"]
-    print(title, content, area_id)
 
-    sql = "INSERT INTO posts (area_id, title, created) VALUES (:area_id, :title, NOW()) RETURNING id"
-    result = db.session.execute(sql, {"area_id":area_id, "title":title})  
+    sql = "INSERT INTO posts (area_id, title, created, user_id) VALUES (:area_id, :title, NOW(), :user_id) RETURNING id"
+    result = db.session.execute(sql, {"area_id":area_id, "title":title, "user_id":user_id})  
     returned_id = result.fetchone()[0]
     db.session.commit()
 
-    sql = "INSERT INTO messages (post_id, content, created) VALUES (:post_id, :content, NOW())"
-    db.session.execute(sql, {"post_id":returned_id, "content":content})
+    sql = "INSERT INTO messages (post_id, content, created, user_id) VALUES (:post_id, :content, NOW(), :user_id)"
+    db.session.execute(sql, {"post_id":returned_id, "content":content, "user_id":user_id})
     db.session.commit()
     
     return redirect(f"/{name}")
@@ -115,8 +126,8 @@ def reply(name:str):
     content = request.form["content"]
     user_id = session["userid"]
 
-    sql = "INSERT INTO messages (post_id, content, created) VALUES (:post_id, :content, NOW())"
-    db.session.execute(sql, {"post_id":post_id, "content":content})
+    sql = "INSERT INTO messages (post_id, content, created, user_id) VALUES (:post_id, :content, NOW(), :user_id)"
+    db.session.execute(sql, {"post_id":post_id, "content":content, "user_id":user_id})
     db.session.commit()
 
     return redirect(f"/{name}/{post_id}")
