cb3d82596a71ce557aed8bb018a71987ef3b43fe
lujada <dariusz.luja@helsinki.fi>
2022-05-04 18:06:31 +0300

    fixed schema.sql

--- schema.sql
+++ schema.sql
@@ -1,5 +1,5 @@
 CREATE TABLE users (id SERIAL PRIMARY KEY, username TEXT UNIQUE, password TEXT, admin BOOLEAN);
 CREATE TABLE areas (id SERIAL PRIMARY KEY, area TEXT, hidden BOOLEAN);
-CREATE TABLE posts (id SERIAL PRIMARY KEY, area_id INTEGER REFERENCES areas(id), title TEXT, created TIMESTAMP, user_id INTEGER REFERENCES user(id));
-CREATE TABLE messages (id SERIAL PRIMARY KEY, post_id INTEGER REFERENCES posts(id), content TEXT, created TIMESTAMP, user_id INTEGER REFERENCES users(id));
-CREATE TABLE photos (id SERIAL PRIMARY KEY, link TEXT, topic_id INTEGER REFERENCES posts(id));
+CREATE TABLE posts (id SERIAL PRIMARY KEY, area_id INTEGER REFERENCES areas, title TEXT, created TIMESTAMP, user_id INTEGER REFERENCES users);
+CREATE TABLE messages (id SERIAL PRIMARY KEY, post_id INTEGER REFERENCES posts, content TEXT, created TIMESTAMP, user_id INTEGER REFERENCES users);
+CREATE TABLE photos (id SERIAL PRIMARY KEY, link TEXT, topic_id INTEGER REFERENCES posts);
