89e617f696c1652c95f2ced8180788551322470b
lujada <dariusz.luja@helsinki.fi>
2022-05-05 09:46:20 +0300

    trying to add logs to heroku

--- db.py
+++ db.py
@@ -2,7 +2,6 @@ from webbrowser import get
 from app import app
 from flask_sqlalchemy import SQLAlchemy
 from os import getenv
-from os import os
 
 app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL").replace("://", "ql://", 1)
 db = SQLAlchemy(app)
