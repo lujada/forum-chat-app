142702988ccca02d972d57529e0e2f2eeef674c2
lujada <dariusz.luja@helsinki.fi>
2022-05-05 17:56:52 +0300

    trying to debug heroku

--- app.py
+++ app.py
@@ -5,9 +5,13 @@ from flask import Flask
 from os import getenv
 import sys
 import logging
+import secrets
 
 app = Flask(__name__)
-app.secret_key = getenv("SECRET_KEY")
+#app.secret_key = getenv("SECRET_KEY")
+secret = secrets.token_urlsafe(32)
+app.secret_key = secret
+
 app.logger.addHandler(logging.StreamHandler(sys.stdout))
 app.logger.setLevel(logging.ERROR)
 import routes
--- db.py
+++ db.py
@@ -4,4 +4,6 @@ from flask_sqlalchemy import SQLAlchemy
 from os import getenv
 
 app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL").replace("://", "ql://", 1)
+
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
